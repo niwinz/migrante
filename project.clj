d427ec6c21cc3388883755747b2fa5878d541f42
Andrey Antukh <niwi@niwi.be>
2015-01-25 20:45:58 +0100

    Minor changes on project.clj (add mapdb dependency)

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[cuerdas "0.3.0"]
                  [slingshot "0.12.1"]
-                 [slingshot "0.10.3"]
-                 [suricatta "0.2.0"]]
-
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                 [suricatta "0.2.0"]
+                 [org.mapdb/mapdb "1.0.6"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [postgresql "9.3-1102.jdbc41"]
                                   [com.h2database/h2 "1.3.176"]]
                    :main ^:skip-aot migrations.test}}
