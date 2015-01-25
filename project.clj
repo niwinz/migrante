5f4aaceb82be20fcb673a478c2161eba79cd8fc0
Andrey Antukh <niwi@niwi.be>
2015-01-25 12:25:44 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject be.niwi/migrations "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Apache 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[datasource "0.1.0"]
+  :license {:name "BSD (2-Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :dependencies [[cuerdas "0.3.0"]
+                 [slingshot "0.12.1"]
                  [slingshot "0.10.3"]
-                 [org.clojure/java.jdbc "0.3.0-beta1"]]
-  :main ^:skip-aot migrations.test
-  :target-path "target/%s"
-  :resource-paths ["config"]
+                 [suricatta "0.2.0"]]
+
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [postgresql "9.1-901.jdbc4"]]}}
-  :migrations {:modules ["migrations/test"]}
+             :dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [postgresql "9.3-1102.jdbc41"]
+                                  [com.h2database/h2 "1.3.176"]]
+                   :main ^:skip-aot migrations.test}}
   :eval-in-leiningen true)
