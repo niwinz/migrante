82eeca100ab270ad84e6a257f3c139446f3723d8
Andrey Antukh <niwi@niwi.be>
2015-01-25 22:53:42 +0100

    Remove mapdb from requirements.
    Now h2 is a default database for store the migrations state.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[cuerdas "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cuerdas "0.3.0"]
                  [slingshot "0.12.1"]
                  [suricatta "0.2.0"]
-                 [org.mapdb/mapdb "1.0.6"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [postgresql "9.3-1102.jdbc41"]
+                 [com.h2database/h2 "1.3.176"]]
+  :profiles {:dev {:dependencies [[postgresql "9.3-1102.jdbc41"]
                                   [com.h2database/h2 "1.3.176"]]
                    :main ^:skip-aot migrations.test}}
   :eval-in-leiningen true)
