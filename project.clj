62986126318806a63774865afec7dfa61ebfddf7
Andrey Antukh <niwi@niwi.be>
2015-01-27 17:48:14 +0100

    Change the project name to migrante.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject be.niwi/migrations "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject migrante "0.1.0-SNAPSHOT"
+  :description "Flexible database migration library for Clojure."
+  :url "https://github.com/niwibe/migrante"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]
