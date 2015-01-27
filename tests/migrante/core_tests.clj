fdf30a8ae8455c7c01c866890cb0ebe7c0066958
Andrey Antukh <niwi@niwi.be>
2015-01-27 18:32:06 +0100

    Uncomment some code on tests.

--- tests/migrante/core_tests.clj
+++ tests/migrante/core_tests.clj
@@ -1,6 +1,7 @@
 (ns migrante.core-tests
   (:require [clojure.test :refer :all]
-            [migrations.core :as mg]))
+            [migrante.core :as mg]
+            [suricatta.core :as sc]))
 
 (def ^{:dynamic true
        :private true}
@@ -11,7 +12,7 @@
 
 (defn- database-fixture
   [end]
-  (with-open [ctx (sc/context pgdbspec)]
+  (with-open [ctx (sc/context dbspec)]
     (sc/atomic ctx
       (binding [*ctx* ctx]
         (end)
@@ -21,28 +22,38 @@
 
 ;; Declaration of migration steps
 
-;; (def migration-0001
-;;   {:up (fn [ctx] (println " (applying migration 0001)"))
-;;    :down (fn [ctx] (println " (unapplying migration 0001)"))})
+(def migration-0001
+  {:up (fn [ctx]
+         (println " (applying migration 0001)"))
+   :down (fn [ctx]
+           (println " (unapplying migration 0001)"))})
 
-;; (def migration-0002
-;;   {:up (fn [ctx] (println " (applying migration 0002)"))
-;;    :down (fn [ctx] (println " (unapplying migration 0002)"))})
+(def migration-0002
+  {:up (fn [ctx]
+         (println " (applying migration 0002)"))
+   :down (fn [ctx]
+           (println " (unapplying migration 0002)"))})
 
-;; (def migration-0003
-;;   {:up (fn [ctx] (println " (applying migration 0003)"))
-;;    :down (fn [ctx] (println " (unapplying migration 0003)"))})
+(def migration-0003
+  {:up (fn [ctx]
+         (println " (applying migration 0003)"))
+   :down (fn [ctx]
+           (println " (unapplying migration 0003)"))})
 
-;; ;; Declaration of migration
+;; Declaration of migration
 
-;; (def migrations
-;;   {:name "testapp"
-;;    :steps [migration-0001
-;;            migration-0002]})
+(def migrations
+  {:name "testapp"
+   :steps [[:0001 migration-0001]
+           [:0002 migration-0002]
+           [:0003 migration-0003]]})
 
-;; ;; Usage example
+;; Usage example
 
-;; (mg/migrate dbspec migrations)
+
+(deftest simple-migrations
+  (mg/migrate dbspec migrations))
+
 ;; (mg/migrate dbspec migrations {:verbose true})
 
 ;; (mg/migrate dbspec migrations {:verbose true
