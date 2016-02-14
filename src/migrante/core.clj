2cc484208e840f4dd74efae70e64645086506573
Andrey Antukh <niwi@niwi.nz>
2016-02-14 17:53:01 +0200

    A little refactor.

--- src/migrante/core.clj
+++ src/migrante/core.clj
@@ -1,14 +1,10 @@
 (ns migrante.core
-  (:require [clojure.pprint :refer (pprint)]
-            [schema.core :as s]
-            [taoensso.timbre :as timbre]
-            [slingshot.slingshot :refer [throw+ try+]]
-            [suricatta.core :as sc]
-            [suricatta.proto :as scproto]))
+  (:require [suricatta.core :as sc]
+            [suricatta.proto :as sp]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Private Api: Helpers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def ^:dynamic *verbose* false)
 
@@ -17,137 +13,108 @@
   into the standard output."
   [& args]
   `(when *verbose*
-     (timbre/info ~@args)))
+     (println ~@args)))
 
-(def ^{:private true
-       :doc "Local migration storage table sql declaration."}
-  bootstrap-sql
-  (str "create table if not exists migrations ("
-       " module varchar(255),"
-       " step varchar(255),"
-       " created_at timestamp,"
-       " unique(module, step)"
-       ");"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Private Api: Validators
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def ^{:private true
-       :doc "A schema representaion of the migration module data structure."}
-  migration-validator
-  (s/checker {:name s/Keyword
-              :steps [[(s/one s/Keyword "stepname")
-                       (s/either (s/pred fn?)
-                                 {:up (s/pred fn?)
-                                  (s/optional-key :down) (s/pred fn?)})]]}))
-
-(defn- migration?
-  "Check if a given parameter is a valid
-  migration data structure or not."
-  [m]
-  (nil? (migration-validator m)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Private Api: Implementation
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn migration-registred?
+(defn- migration-registered?
   "Check if concrete migration is already registred."
-  ([ctx module step]
+  ([conn module step]
    {:pre [(keyword? module) (keyword? step)]}
    (let [sql (str "select * from migrations"
                   " where module=? and step=?")
-         res (sc/fetch ctx [sql (name module) (name step)])]
+         res (sc/fetch conn [sql (name module) (name step)])]
      (pos? (count res)))))
 
 (defn- register-migration!
   "Register a concrete migration into local migrations database."
-  ([ctx module step]
+  ([conn module step]
    {:pre [(keyword? module) (keyword? step)]}
    (let [sql "insert into migrations (module, step) values (?, ?)"]
-     (sc/execute ctx [sql (name module) (name step)]))))
+     (sc/execute conn [sql (name module) (name step)]))))
 
 (defn- unregister-migration!
   "Unregister a concrete migration from local migrations database."
-  ([ctx module step]
+  ([conn module step]
    {:pre [(keyword? module) (keyword? step)]}
    (let [sql "delete from migrations where module=? and step=?;"]
-     (sc/execute ctx [sql (name module) (name step)]))))
+     (sc/execute conn [sql (name module) (name step)]))))
 
-(defn local-context
-  "Return a local context (used for stroe the migrations
-  state of the application."
-  [{:keys [localdb] :or {localdb "_migrations.h2"}}]
-  (let [dbspec {:subprotocol "h2" :subname localdb}
-        ctx    (sc/context dbspec)]
-    (sc/execute ctx bootstrap-sql)
-    ctx))
+(defn- setup!
+  "Initialize the database if it is not initialized."
+  [conn]
+  (let [sql (str "create table if not exists migrations ("
+                 " module varchar(255),"
+                 " step varchar(255),"
+                 " created_at timestamp,"
+                 " unique(module, step)"
+                 ");")]
+    (sc/execute conn sql)))
 
 (defprotocol IMigration
   "Define a migration step behavior on up and down
   migration actons."
-  (^:private run-up [_ _] "Run function in migrate process.")
-  (^:private run-down [_ _] "Run function in rollback process."))
+  (-run-up [_ _] "Run function in migrate process.")
+  (-run-down [_ _] "Run function in rollback process."))
 
 (extend-protocol IMigration
   clojure.lang.IPersistentMap
-  (run-up [step ctx]
+  (-run-up [step conn]
     (let [upfn (:up step)]
-      (upfn ctx)))
-  (run-down [step ctx]
+      (upfn conn)))
+  (-run-down [step conn]
     (if-let [downfn (:down step)]
-      (downfn ctx)))
+      (downfn conn)))
 
   clojure.lang.IFn
-  (run-up [step ctx]
-    (step ctx))
-  (run-down [step ctx]
+  (-run-up [step conn]
+    (step conn))
+  (-run-down [step conn]
     nil))
 
 (defn- do-migrate
-  [mctx lctx migration {:keys [until fake] :or {fake false}}]
+  [conn migration {:keys [until fake] :or {fake false}}]
   (let [mid (:name migration)
         steps (:steps migration)]
-    (sc/atomic mctx
-      (reduce (fn [_ [sid sdata]]
-                (when-not (migration-registred? lctx mid sid)
-                  (log (format "- Applying migration %s/%s." mid sid))
-                  (sc/atomic mctx
-                    (when (not fake)
-                      (run-up sdata mctx))
-                    (register-migration! lctx mid sid)))
-                (when (= until sid)
-                  (reduced nil)))
-              nil
-              steps))))
+    (sc/atomic conn
+      (run! (fn [[sid sdata]]
+              (when-not (migration-registered? conn mid sid)
+                (log (format "- Applying migration %s/%s" mid sid))
+                (sc/atomic conn
+                  (when (not fake)
+                    (-run-up sdata conn))
+                  (register-migration! conn mid sid)))
+              (when (= until sid)
+                (reduced nil)))
+            steps))))
 
 (defn- do-rollback
-  [mctx lctx migration {:keys [until fake] :or {fake false}}]
+  [conn migration {:keys [until fake] :or {fake false}}]
   (let [mid (:name migration)
         steps (reverse (:steps migration))]
-    (sc/atomic mctx
-      (reduce (fn [_ [sid sdata]]
-                (when (migration-registred? lctx mid sid)
-                  (log (format "- Rollback migration %s/%s." mid sid))
-                  (sc/atomic mctx
-                    (when (not fake)
-                      (run-down sdata mctx))
-                    (unregister-migration! lctx mid sid)))
-                (when (= until sid)
-                  (reduced nil)))
-              nil
-              steps))))
+    (sc/atomic conn
+      (run! (fn [[sid sdata]]
+              (when (migration-registered? conn mid sid)
+                (log (format "- Rollback migration %s/%s" mid sid))
+                (sc/atomic conn
+                  (when (not fake)
+                    (-run-down sdata conn))
+                  (unregister-migration! conn mid sid)))
+              (when (= until sid)
+                (reduced nil)))
+            steps))))
 
-(defn- normalize-to-context
+(defn- normalize-to-connection
   [dbspec]
-  (if (satisfies? scproto/IContext dbspec)
+  (if (satisfies? sp/IContextHolder dbspec)
     dbspec
     (sc/context dbspec)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Public Api
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn execute
   "Execute a query and return a number of rows affected."
@@ -167,24 +134,43 @@
   ([ctx q opts]
    (sc/fetch ctx q opts)))
 
+(defprotocol IMigrationContext
+  (-migrate [_ migration options])
+  (-rollback [_ migration options])
+  (-registered? [_ module step]))
+
+(defn context
+  "Create new instance of migration context."
+  ([conn] (context conn nil))
+  ([conn {:keys [verbose] :or {verbose true}}]
+   (let [conn (normalize-to-connection conn)]
+     (setup! conn)
+     (reify IMigrationContext
+       (-migrate [_ migration options]
+         (sc/atomic conn
+           (binding [*verbose* verbose]
+             (do-migrate conn migration options))))
+       (-rollback [_ migration options]
+         (sc/atomic conn
+           (binding [*verbose* verbose]
+             (do-rollback conn migration options))))
+       (-registered? [_ module step]
+         (migration-registered? conn module step))))))
+
 (defn migrate
   "Main entry point for apply a migration."
-  ([dbspec migration] (migrate dbspec migration {}))
-  ([dbspec migration {:keys [verbose] :or {verbose true} :as options}]
-   {:pre [(migration? migration)]}
-   (with-open [mctx (normalize-to-context dbspec)
-               lctx (local-context options)]
-     (sc/atomic lctx
-       (binding [*verbose* verbose]
-         (do-migrate mctx lctx migration options))))))
+  ([mctx migration]
+   (migrate mctx migration nil))
+  ([mctx migration options]
+   (-migrate mctx migration options)))
 
 (defn rollback
-  "Main entry point for rollback migrations."
-  ([dbspec migration] (rollback dbspec migration {}))
-  ([dbspec migration {:keys [verbose] :or {verbose true} :as options}]
-   {:pre [(migration? migration)]}
-   (with-open [mctx (normalize-to-context dbspec)
-               lctx (local-context options)]
-     (sc/atomic lctx
-       (binding [*verbose* verbose]
-         (do-rollback mctx lctx migration options))))))
+  "Main entry point for apply a migration."
+  ([mctx migration]
+   (rollback mctx migration nil))
+  ([mctx migration options]
+   (-rollback mctx migration options)))
+
+(defn registered?
+  [mctx module step]
+  (-registered? mctx module step))
